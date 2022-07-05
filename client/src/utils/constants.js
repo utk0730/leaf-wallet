2efa0b7347e46680ebb7ddf1649f64a47a9c65fc
utkarsh <naruto@Narutos-MacBook-Air.local>
2022-07-05 10:51:23 +0530

    hardhat config and contract deployment using alchemy

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import TransactionABI from "./transactions.json";
+export const transactionABI = TransactionABI;
+export const transactionsContractAddress =
+  "0xa5272154625532494559a55606193be6d30b5633";
--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -1,5 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract Transactions{
+    uint transactionCount;
+    struct TransactionStruct {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+    event Transfer( 
+        address sender,
+        address receiver,
+        uint amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+        );
+    
+   TransactionStruct[] transactions;
+
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+        transactionCount +=1;
+        transactions.push(TransactionStruct(msg.sender,receiver, amount, message,block.timestamp,keyword));
+        emit Transfer(msg.sender,receiver, amount, message,block.timestamp,keyword);
+        
+
+    }
+    function getAllTransactions() public view returns(TransactionStruct[] memory){
+        return transactions;
+    }
+    function getTransactionCount() public view returns(uint256 ){
+        return transactionCount;
+    }
 
 }
